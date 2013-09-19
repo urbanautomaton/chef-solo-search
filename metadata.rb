681f78a108f5f8a3655551b32d9939603628c568
Markus Korn <markus.korn@edelight.de>
2013-09-19 11:14:10 +0200

    0.5.1 BUGFIX-RELEASE: added missing metadata.json inoder to make the cookbook releaseable in community.opscode.com

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "markus.korn@edelight.de"
 license          "Apache 2.0"
 description      "Data bag search for Chef Solo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.0"
+version          "0.5.1"
 
 %w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
